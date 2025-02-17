12d219657d4b3d4b7af23974f0ea4fb582ca08d9
Zaara Khan <45593854+ZaaraKhan@users.noreply.github.com>
2025-02-16 16:02:32 -0800

    coloring and adding logos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import resumeLogo from "./assets/resume.svg";
+import aboutLogo from "./assets/about.svg";
+import projectsLogo from "./assets/projects.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+      </div> */}
+      <div>
+        <h1>Zaara Khan</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+
+      <div className="container">
+        <a className="item">
+          <img src={resumeLogo} className="logo" />
+          Resumé
+        </a>
+
+        <a className="item">
+          <img src={aboutLogo} className="logo" />
+          <a>About</a>
+        </a>
+
+        <a className="item">
+          <img src={projectsLogo} className="logo" />
+          <a>Projects</a>
+        </a>
+      </div>
+
+      {/* <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
@@ -27,9 +50,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
