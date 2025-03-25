edaf04574cdaa808b90cdad0e73005c30fa297ba
Zaara Khan <45593854+ZaaraKhan@users.noreply.github.com>
2025-03-25 16:40:21 -0700

    clean up projects page

--- src/Projects.tsx
+++ src/Projects.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import backIcon from "./assets/back.svg";
+import "./Projects.css";
 
 function Projects() {
   return (
@@ -8,19 +9,28 @@ function Projects() {
         <h1>Zaara Khan</h1>
       </Link>
       <div className="info">
+        <br />
         <a
           href="https://vercel.com/zaaras-projects-3f77e5c8/poetry-app/5cdP2azLFT2g5Vd7biBMaqJzWZK5"
           target="_blank"
+          className="project"
         >
-          Poetry Generator made with Google Gemini API
+          Poetry Generator
         </a>
-        <br />
+        <p>
+          Made with Google Gemini API to answer the question, what is Modern
+          Art?
+        </p>
         <a
           href="https://github.com/ZaaraKhan/poetry-app/tree/main"
           target="_blank"
+          className="smallLink"
         >
           Link to GitHub repo
         </a>
+        <br />
+        <br />
+        <br />
         <div className="back">
           <Link to="/home">
             <img src={backIcon} className="back"></img>
