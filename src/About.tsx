936cdf9b941d84a7e0f8a3de158662b0ee08b10b
Zaara Khan <45593854+ZaaraKhan@users.noreply.github.com>
2026-01-29 14:40:39 -0800

    change font, add img, add hover animation in about

--- src/About.tsx
+++ src/About.tsx
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
-import backIcon from "./assets/back.svg";
+import { useState } from "react";
 import "./About.css";
 
 function About() {
+  const [activeWords, setActiveWords] = useState(new Set());
+  const activateWord = (word: string) => {
+    setActiveWords((prev) => {
+      const next = new Set(prev);
+      next.add(word);
+      return next;
+    });
+  };
+
   return (
     <div className="container">
       <div className="title">
@@ -10,42 +19,136 @@ function About() {
           <h1>Zaara Khan</h1>
         </Link>
       </div>
-      <div className="about info">
-        <br />
-        <div className="quote">
+      <div className="about">
+        <div className="image">
+          <img src="src/assets/bioImage.jpg"></img>
+        </div>
+        <div className="text">
           {" "}
-          <h2>“Who is to say that pleasure is useless?”</h2>
-          <p>Charles Eames</p>
-        </div>
-        <br /> <br />
-        <p>
-          Zaara Khan is a designer and software engineer specializing in
-          accessible, well-designed frontend interfaces. She believes form
-          follows function, while leaving room for delight. <br /> <br />
-          Zaara is currently working on freelance UX design projects.
-          Previously, she worked as a frontend engineer at Simplr.AI, now
-          Asurion, building customer service software. She holds a B.S. in
-          Cognitive Science from UC San Diego. She is inspired by modernist
-          architecture, small websites, and unique color combinations. <br />{" "}
           <br />
-          Zaara lives in San Francisco, California, where she spends her spare
-          time at the pottery studio, volunteering, or drawing at a local park.
-          She is open to creative new opportunities, especially those
-          prioritizing positive social impact. You can reach her at{" "}
-          <a href="mailto:zaarakay21@gmail.com">zaarakay21@gmail.com</a> or on{" "}
-          <a href="https://www.linkedin.com/in/zaarak/" target="_blank">
-            LinkedIn
-          </a>
-          .
-        </p>
-        <br />
-        <br />
-        <br />
-        <div className="back">
-          <a href="/home">
-            <div className="backIcon"></div>
-          </a>
+          <div className="quote">
+            {" "}
+            <h2>
+              <span
+                className={`icon-link ${
+                  activeWords.has("quote") ? "active" : ""
+                }`}
+                onMouseEnter={() => activateWord("quote")}
+                onFocus={() => activateWord("quote")}
+              >
+                “Who is to say that pleasure is useless?”
+              </span>
+            </h2>
+            <p>Charles Eames</p>
+          </div>
+          <br /> <br />
+          <p>
+            Zaara Khan is a{" "}
+            <span
+              className={`icon-link ${
+                activeWords.has("designer") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("designer")}
+              onFocus={() => activateWord("designer")}
+            >
+              designer
+            </span>{" "}
+            and{" "}
+            <span
+              className={`icon-link ${
+                activeWords.has("engineer") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("engineer")}
+              onFocus={() => activateWord("engineer")}
+            >
+              software engineer
+            </span>{" "}
+            specializing in accessible, well-designed frontend interfaces. She
+            believes form follows function, while leaving room for delight.{" "}
+            <br /> <br />
+            Zaara is currently working on freelance UX design projects.
+            Previously, she worked as a{" "}
+            <span
+              className={`icon-link ${
+                activeWords.has("frontend") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("frontend")}
+              onFocus={() => activateWord("frontend")}
+            >
+              frontend engineer
+            </span>{" "}
+            at Simplr.AI, now{" "}
+            <span
+              className={`icon-link ${
+                activeWords.has("asurion") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("asurion")}
+              onFocus={() => activateWord("asurion")}
+            >
+              Asurion
+            </span>
+            , building customer service software. She holds a B.S. in{" "}
+            <span
+              className={`icon-link ${
+                activeWords.has("cogsci") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("cogsci")}
+              onFocus={() => activateWord("cogsci")}
+            >
+              Cognitive Science
+            </span>{" "}
+            from UC San Diego. She is inspired by modernist architecture, small
+            websites, and unique color combinations. <br /> <br />
+            Zaara lives in{" "}
+            <span
+              className={`icon-link ${activeWords.has("sf") ? "active" : ""}`}
+              onMouseEnter={() => activateWord("sf")}
+              onFocus={() => activateWord("sf")}
+            >
+              San Francisco
+            </span>
+            , California, where she spends her spare time at the pottery studio,
+            volunteering, or drawing at a local park. She is open to creative
+            new opportunities, especially those prioritizing{" "}
+            <span
+              className={`icon-link ${
+                activeWords.has("positive") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("positive")}
+              onFocus={() => activateWord("positive")}
+            >
+              positive social impact.
+            </span>{" "}
+            You can reach her at{" "}
+            <a
+              href="mailto:zaarakay21@gmail.com"
+              className={`icon-link ${
+                activeWords.has("email") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("email")}
+              onFocus={() => activateWord("email")}
+            >
+              zaarakay21@gmail.com
+            </a>{" "}
+            or on{" "}
+            <a
+              href="https://www.linkedin.com/in/zaarak/"
+              target="_blank"
+              className={`icon-link ${
+                activeWords.has("linkedin") ? "active" : ""
+              }`}
+              onMouseEnter={() => activateWord("linkedin")}
+              onFocus={() => activateWord("linkedin")}
+            >
+              LinkedIn
+            </a>
+            .
+          </p>
+          <br />
+          <br />
+          <br />
         </div>
+
         {/* <div className="footer">
           {" "}
           This website was designed and coded by Zaara Khan with React, CSS,
@@ -59,6 +162,11 @@ function About() {
           </a>{" "}
           © 2026
         </div> */}
+      </div>
+      <div className="back">
+        <a href="/home">
+          <div className="backIcon"></div>
+        </a>
       </div>
     </div>
   );
